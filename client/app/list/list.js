fe7be8c90dab5a5b02ffc49d0d1c06d3df88c367
Henry Pötzl <Henry.Poetzl@jambit.com>
2014-09-08 17:20:21 +0200

    Connect AngularJS client with mongoDB

--- client/app/list/list.js
+++ client/app/list/list.js
@@ -3,12 +3,14 @@
 angular.module('coffee_shop.list', ['ngRoute'])
 
 .config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/list', {
-    templateUrl: 'list/list.html',
-    controller: 'ListCtrl'
-  });
+	$routeProvider.when('/list', {
+		templateUrl: 'list/list.html',
+		controller: 'ListCtrl'
+	});
 }])
 
-.controller('ListCtrl', [function() {
-
+.controller('ListCtrl', ['$scope', '$http', function($scope, $http) {
+	$http.get('/coffee').success(function(data) {
+		$scope.coffees = data;
+	});
 }]);
